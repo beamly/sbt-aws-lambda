bd46ef6770614eaec785923af86d5f36631ad7f6
Dale Wijnand <dale.wijnand@gmail.com>
2016-07-13 12:44:13 +0100

    Switch AwsCredentials to use the default auth chain

--- src/main/scala/com/gilt/aws/lambda/AwsCredentials.scala
+++ src/main/scala/com/gilt/aws/lambda/AwsCredentials.scala
@@ -1,7 +1,21 @@
 package com.gilt.aws.lambda
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider
+import com.amazonaws._, auth._, profile._
 
 private[lambda] object AwsCredentials {
-  lazy val provider = new ProfileCredentialsProvider()
+  lazy val provider: AWSCredentialsProvider =
+    new AWSCredentialsProviderChain(
+      new EnvironmentVariableCredentialsProvider,
+      new SystemPropertiesCredentialsProvider,
+      new ProfileCredentialsProvider,
+      new InstanceProfileCredentialsProvider
+    ) {
+
+    override def getCredentials: AWSCredentials = {
+      try return super.getCredentials
+      catch { case ace: AmazonClientException => }
+      println("No credentials available; falling back to anonymous access")
+      null
+    }
+  }
 }
